c572b1e83859eea73a42519199f15c30cfa81702
Congson2002 <71306485+Congson2002@users.noreply.github.com>
2020-09-17 01:30:38 -0700

    Update bai-5.php

--- bai-5.php
+++ bai-5.php
@@ -1,37 +1,5 @@
 <?php
 echo "Kết quả in ra: ";
-$i ="apple";
-switch ($i) {
-    case "banana":
-        echo "i is banana";
-        break;
-    case "orange":
-        echo "i is orange";
-        break;
-    case "mango":
-        echo "i is mango"; 
-        break;
-    case "apple":
-        echo "i is apple ";
-        break;
-}
-
-echo "Day la ket qua tinh giai thua cua 5: ";
-
-$result=1;
-$i = 5;
-do {
-    $result*=$i;
-    $i--;
-} while ($i > 0);
- 
-echo $result;
-
-echo "Kết quả";
-for ($x = 0; $x <=10; $x++) {
-    echo "The number is: $x <br>";
-}
-echo "Kết quả in ra: ";
 $a = 2;
 $b = 2;
     if ($a > $b) {
@@ -41,4 +9,55 @@ $b = 2;
 }   else {
         echo "a is smaller than b";
 }
+echo ('<br /> ------------------------------------------ End Vd1. <br />');
+echo "Kết quả in ra: ";
+$i ="apple";
+switch ($i) {
+    case "banana":
+        echo "it is banana";
+        break;
+    case "orange":
+        echo "it is orange";
+        break;
+    case "mango":
+        echo "it is mango"; 
+        break;
+    case "apple":
+        echo "it is apple ";
+        break;
+}
+echo ('<br /> ------------------------------------------ End Vd2. <br />');
+echo ('Kết quả in ra: ');
+echo ('<br />');
+$i = 1;
+while ($i <= 3) {
+    $i++;
+}
+echo "value of variable i: ".$i."\n";
+echo ('<br />');
+$j = 1;
+while ($j <= 3):
+    $j++;
+endwhile;
+echo "value of variable j: ".$i;
+echo ('<br /> ------------------------------------------ End Vd3. <br />');
+echo "Tính giai thừa của 10: ";
+echo ('<br />');
+echo ('Kết quả in ra: ');
+$result=1;
+$i = 10;
+do {
+    $result*=$i;
+    $i--;
+} while ($i > 0);
+ 
+echo $result;
+echo ('<br /> ------------------------------------------ End Vd4. <br />');
+echo "Kết quả in ra: ";
+echo ('<br />');
+for ($x = 0; $x <=10; $x++) {
+    echo "The number is: $x <br>";
+}
+echo ('<br /> ------------------------------------------ End Vd5. <br />');
+
 ?>

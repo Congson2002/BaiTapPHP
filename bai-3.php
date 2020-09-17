857995b11153547ad7d385fe9d8538853aca6636
Congson2002 <71306485+Congson2002@users.noreply.github.com>
2020-09-17 00:52:57 -0700

    Update bai-3.php

--- bai-3.php
+++ bai-3.php
@@ -2,17 +2,19 @@
 $y = 10;
 function assign_y () {
 $y = 0;
-print "\$y inside function is  $y.
-";
+print "\$y inside function is  $y.";
+echo ('<br />');
 }
 assign_y();
-print "\$y outside of function is $y.
-";
-echo "Kết quả in ra: "; 
+print "\$y outside of function is $y.";
+echo ('<br /> ---------------------------------- End Vd1. <br />');
 function multiply ($value) {
     $value = $value * 10;
     return $value;
 }
 $retval = multiply  (50);
+echo ('Kết quả in ra: ');
+echo ('<br />');
 Print "Return value is $retval\n";
+echo ('<br /> ---------------------------------- End Vd2. <br />');
 ?>  
